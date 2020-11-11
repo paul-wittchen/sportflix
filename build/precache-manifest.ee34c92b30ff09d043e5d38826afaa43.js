04db8ea9f5f8ca3803318abeb661748d24274fee
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-11 15:58:01 +0100

    new build folder

---
+++ build/precache-manifest.ee34c92b30ff09d043e5d38826afaa43.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fdadb3ff5c505e28edfaac668d3701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "587e066a869c7a738ac8",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1d0a910df73399120f27",
+    "url": "/static/css/main.359f5057.chunk.css"
+  },
+  {
+    "revision": "587e066a869c7a738ac8",
+    "url": "/static/js/2.522c74cf.chunk.js"
+  },
+  {
+    "revision": "42f1207e367a0f0a273c20b8e10e1361",
+    "url": "/static/js/2.522c74cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0a910df73399120f27",
+    "url": "/static/js/main.2456ebd5.chunk.js"
+  },
+  {
+    "revision": "2021d37780107328078a",
+    "url": "/static/js/runtime-main.f6f1ccde.js"
+  },
+  {
+    "revision": "9d28ea7d00792686b7af03944d4cbed4",
+    "url": "/static/media/3bl.9d28ea7d.png"
+  },
+  {
+    "revision": "b23007f957bd72d3cf146cc19ddbe8ea",
+    "url": "/static/media/bl.b23007f9.png"
+  },
+  {
+    "revision": "6b0b0cf932a498508aa033d5067cc821",
+    "url": "/static/media/cl.6b0b0cf9.png"
+  },
+  {
+    "revision": "47709a6eb714de9faa8ebb7c211a694d",
+    "url": "/static/media/ere.47709a6e.png"
+  },
+  {
+    "revision": "a645a71d03cdd3703963632c501907e9",
+    "url": "/static/media/lig.a645a71d.png"
+  },
+  {
+    "revision": "031f015e3be475fd866f3c09a4ffe597",
+    "url": "/static/media/ligue1.031f015e.png"
+  },
+  {
+    "revision": "f9679dc382b9bcaa8342298282e4cd87",
+    "url": "/static/media/mls.f9679dc3.png"
+  },
+  {
+    "revision": "dcf00a7e920e2d4394b8b326ef72a563",
+    "url": "/static/media/nfl.dcf00a7e.png"
+  },
+  {
+    "revision": "431edcbffe7db3dcc6bf66d6fbf5d05a",
+    "url": "/static/media/sportflix-logo.431edcbf.png"
+  }
+]);
