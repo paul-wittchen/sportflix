169826a34a7bb587f26400581a057649ea4f8498
Paul Wittchen <wittchenpaul@gmail.com>
2020-11-11 15:05:46 +0100

    bug fixed

--- src/components/liga.js
+++ src/components/liga.js
@@ -34,93 +34,97 @@ export default class Liga extends Component {
                 <Spinner animation="grow" variant="light" />
             )
         } else {
-            if (this.state.scroll === 1) {
-                return(
-                    <div className='leagueContainer'>
-                        <Row>
-                            <Col md={4}><p className='leagueTitle'>{this.state.data[0].strLeague}</p></Col>
-                            <Col md={{ span: 4, offset: 4 }} className='arrows'>
-                                <i className="fas fa-chevron-right fa-2x" onClick={this.plusOne}></i>
-                                <i className="fas fa-chevron-left fa-2x"></i>
-                            </Col>
-                        </Row>
-                        <Row lg={6} md={4} sm={2} className='leagueCards'>
-                            {this.state.data.slice(0,6).map(item => (
-                                <GenerateCard
-                                    img={item.strThumb}
-                                    key={item.strThumb}
-                                    alt={item.strEvent}
-                                    homeTeam={item.strHomeTeam}
-                                    awayTeam={item.strAwayTeam}
-                                    date={item.dateEvent}
-                                    time={item.strTime}
-                                    comp={item.strLeague}
-                                    round={item.intRound}
-                                    homeTeamId={item.idHomeTeam}
-                                    awayTeamId={item.idAwayTeam}
-                                />
-                            ))}
-                        </Row>
-                    </div>
-                )
-            } else if (this.state.scroll === 2) {
-                return(
-                    <div className='leagueContainer'>
-                        <Row className='test1'>
-                            <Col lg={4}><p className='leagueTitle'>{this.state.data[0].strLeague}</p></Col>
-                            <Col lg={{ span: 4, offset: 4 }}>
-                                <i className="fas fa-chevron-right fa-2x" onClick={this.plusOne}></i>
-                                <i className="fas fa-chevron-left fa-2x" onClick={this.minusOne}></i>
-                            </Col>
-                        </Row>
-                        <Row lg={6} md={4} sm={2} className='leagueCards'>
-                            {this.state.data.slice(7,13).map(item => (
-                                <GenerateCard
-                                    img={item.strThumb}
-                                    key={item.strThumb}
-                                    alt={item.strEvent}
-                                    homeTeam={item.strHomeTeam}
-                                    awayTeam={item.strAwayTeam}
-                                    date={item.dateEvent}
-                                    time={item.strTime}
-                                    comp={item.strLeague}
-                                    round={item.intRound}
-                                    homeTeamId={item.idHomeTeam}
-                                    awayTeamId={item.idAwayTeam}
-                                />
-                            ))}
-                        </Row>
-                    </div>
-                )
+            if (this.state.data === null) {
+                return(<></>)
             } else {
-                return(
-                    <div className='leagueContainer'>
-                        <Row>
-                            <Col md={4}><p className='leagueTitle'>{this.state.data[0].strLeague}</p></Col>
-                            <Col md={{ span: 4, offset: 4 }}>
-                                <i className="fas fa-chevron-right fa-2x"></i>
-                                <i className="fas fa-chevron-left fa-2x" onClick={this.minusOne}></i>
-                            </Col>
-                        </Row>
-                        <Row lg={6} md={4} sm={2} className='leagueCards'>
-                            {this.state.data.slice(14,20).map(item => (
-                                <GenerateCard
-                                    img={item.strThumb}
-                                    key={item.strThumb}
-                                    alt={item.strEvent}
-                                    homeTeam={item.strHomeTeam}
-                                    awayTeam={item.strAwayTeam}
-                                    date={item.dateEvent}
-                                    time={item.strTime}
-                                    comp={item.strLeague}
-                                    round={item.intRound}
-                                    homeTeamId={item.idHomeTeam}
-                                    awayTeamId={item.idAwayTeam}
-                                />
-                            ))}
-                        </Row>
-                    </div>
-                )
+                if (this.state.scroll === 1) {
+                    return(
+                        <div className='leagueContainer'>
+                            <Row>
+                                <Col md={4}><p className='leagueTitle'>{this.state.data[0].strLeague}</p></Col>
+                                <Col md={{ span: 4, offset: 4 }} className='arrows'>
+                                    <i className="fas fa-chevron-right fa-2x" onClick={this.plusOne}></i>
+                                    <i className="fas fa-chevron-left fa-2x"></i>
+                                </Col>
+                            </Row>
+                            <Row lg={6} md={4} sm={2} className='leagueCards'>
+                                {this.state.data.slice(0,6).map(item => (
+                                    <GenerateCard
+                                        img={item.strThumb}
+                                        key={item.strThumb}
+                                        alt={item.strEvent}
+                                        homeTeam={item.strHomeTeam}
+                                        awayTeam={item.strAwayTeam}
+                                        date={item.dateEvent}
+                                        time={item.strTime}
+                                        comp={item.strLeague}
+                                        round={item.intRound}
+                                        homeTeamId={item.idHomeTeam}
+                                        awayTeamId={item.idAwayTeam}
+                                    />
+                                ))}
+                            </Row>
+                        </div>
+                    )
+                } else if (this.state.scroll === 2) {
+                    return(
+                        <div className='leagueContainer'>
+                            <Row className='test1'>
+                                <Col lg={4}><p className='leagueTitle'>{this.state.data[0].strLeague}</p></Col>
+                                <Col lg={{ span: 4, offset: 4 }}>
+                                    <i className="fas fa-chevron-right fa-2x" onClick={this.plusOne}></i>
+                                    <i className="fas fa-chevron-left fa-2x" onClick={this.minusOne}></i>
+                                </Col>
+                            </Row>
+                            <Row lg={6} md={4} sm={2} className='leagueCards'>
+                                {this.state.data.slice(7,13).map(item => (
+                                    <GenerateCard
+                                        img={item.strThumb}
+                                        key={item.strThumb}
+                                        alt={item.strEvent}
+                                        homeTeam={item.strHomeTeam}
+                                        awayTeam={item.strAwayTeam}
+                                        date={item.dateEvent}
+                                        time={item.strTime}
+                                        comp={item.strLeague}
+                                        round={item.intRound}
+                                        homeTeamId={item.idHomeTeam}
+                                        awayTeamId={item.idAwayTeam}
+                                    />
+                                ))}
+                            </Row>
+                        </div>
+                    )
+                } else {
+                    return(
+                        <div className='leagueContainer'>
+                            <Row>
+                                <Col md={4}><p className='leagueTitle'>{this.state.data[0].strLeague}</p></Col>
+                                <Col md={{ span: 4, offset: 4 }}>
+                                    <i className="fas fa-chevron-right fa-2x"></i>
+                                    <i className="fas fa-chevron-left fa-2x" onClick={this.minusOne}></i>
+                                </Col>
+                            </Row>
+                            <Row lg={6} md={4} sm={2} className='leagueCards'>
+                                {this.state.data.slice(14,20).map(item => (
+                                    <GenerateCard
+                                        img={item.strThumb}
+                                        key={item.strThumb}
+                                        alt={item.strEvent}
+                                        homeTeam={item.strHomeTeam}
+                                        awayTeam={item.strAwayTeam}
+                                        date={item.dateEvent}
+                                        time={item.strTime}
+                                        comp={item.strLeague}
+                                        round={item.intRound}
+                                        homeTeamId={item.idHomeTeam}
+                                        awayTeamId={item.idAwayTeam}
+                                    />
+                                ))}
+                            </Row>
+                        </div>
+                    )
+                }
             }
         }
     }
